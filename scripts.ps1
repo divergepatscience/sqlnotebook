961a2c8dca628f926c856ac25cbaa631d0f63701
Brian Luft <brian@electroly.com>
2019-01-20 22:39:44 -0400

    Use electron?

--- scripts.ps1
+++ scripts.ps1
@@ -1,26 +1,29 @@
-# from the PowerShell prompt:
-# . .\scripts.ps1
+#Requires -Version 5
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+
+# from the PowerShell prompt, import these functions using: . .\scripts.ps1
 
 function Build {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/build.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/build.ps1')).Path
 }
 
 function Format {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/format/format.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/format/format.ps1')).Path
 }
 
 function Run {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/run.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/run.ps1')).Path
 }
 
 function Clean {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/clean.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/clean.ps1')).Path
 }
 
 function Web {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/web.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/web.ps1')).Path
 }
 
 function RunWeb {
-    & (Resolve-Path (Join-Path $PSScriptRoot 'scripts/runweb.ps1')).Path
+    & (Resolve-Path (Join-Path $PSScriptRoot 'ps1/runweb.ps1')).Path
 }
